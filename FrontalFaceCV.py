46040a14c1f558d64d96d6cb68bd4718bbc84953
Suhan Koh <suhankoh@gmail.com>
2016-11-06 20:37:41 -0800

    Added python script

---
+++ FrontalFaceCV.py
@@ -0,0 +1,48 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+import numpy
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.rotation = 180
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+# allow the camera to warmup
+time.sleep(0.1)
+
+#Load a cascade file for detecting faces
+face_cascade = cv2.CascadeClassifier('/home/pi/Desktop/faces.xml')
+
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
+
+    #Convert to grayscale
+    gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+    #Look for faces in the image using the loaded cascade file
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+
+    print("Found "+str(len(faces))+" face(s)")
+
+    #Draw a rectangle around every found face
+    for (x,y,w,h) in faces:
+        cv2.rectangle(image,(x,y),(x+w,y+h),(255,255,0),2)
+
+    # show the frame
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
